8a4aee343ccba7b700c1d091a8315597787b7cff
Magdalena Dusza <magdalena.dusza@glovoapp.com>
2021-10-03 22:10:52 +0200

    add restaurant select input

---
+++ components/AppSelect.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <label :for="select">What {{select}} do you like?</label>
+        <select @change="updateValue" :name="select" :id="select">
+            <option value>-- Please select a {{select}} --</option>
+            <option v-for="option in selectOptions" :key="option" :value="option">{{option}}</option>
+        </select>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        select: {
+            type: String,
+            default: 'cuisine'
+        },
+        selectOptions: {
+            type: Array,
+            default: () => ['tacos', 'pizza', 'dim sum']
+        }
+    },
+    methods: {
+        updateValue(event){
+            this.$emit('changeSelect', event.target.value)
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -2,16 +2,30 @@
 <main class="container restaurant">
     <div class="restaurantheading">
         <h1>Restaurants:</h1>
+        <app-select @changeSelect="updateSelectedRestaurant"/>
+        <pre>{{$data}}</pre>
     </div>
     <app-restaurant-info/>
 </main>
 </template>
 <script>
 import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue'
+import AppSelect from '@/components/AppSelect.vue'
 export default {
     components: {
-        AppRestaurantInfo
+        AppRestaurantInfo,
+        AppSelect
 
+    },
+    data(){
+        return {
+            selectedRestaurant: ''
+        }
+    },
+    methods: {
+        updateSelectedRestaurant(val){
+            this.selectedRestaurant = val;
+        }
     }
 }
 </script>
